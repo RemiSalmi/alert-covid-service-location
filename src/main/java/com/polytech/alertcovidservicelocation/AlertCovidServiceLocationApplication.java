6ac6550c429ed021fce4bdef8d48e3dcb5558909
Remi Salmi <remi.salmi@gmail.com>
2020-10-24 17:17:36 +0200

    init project

---
+++ src/main/java/com/polytech/alertcovidservicelocation/AlertCovidServiceLocationApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidservicelocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCovidServiceLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCovidServiceLocationApplication.class, args);
+	}
+
+}
