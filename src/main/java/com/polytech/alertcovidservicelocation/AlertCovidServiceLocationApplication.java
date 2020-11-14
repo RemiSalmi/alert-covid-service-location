ac67b14dec94cb0c29462672c1921d1c1a91a8f7
Remi Salmi <remi.salmi@gmail.com>
2020-11-14 16:04:58 +0100

    add cors

--- src/main/java/com/polytech/alertcovidservicelocation/AlertCovidServiceLocationApplication.java
+++ src/main/java/com/polytech/alertcovidservicelocation/AlertCovidServiceLocationApplication.java
@@ -2,12 +2,28 @@ package com.polytech.alertcovidservicelocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlertCovidServiceLocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlertCovidServiceLocationApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
